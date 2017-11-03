6f7078bc35992c2ca2667b9e1d791297d1e1bcd5
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-11-02 23:57:44 -0700

    Add files via upload

---
+++ Lesson 2 - Data Types- Variables and Operators/SimpleCalculatorChallenge/SimpleCalculatorChallenge/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleCalculatorChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style3 {
+            font-size: medium;
+            background-color: #99CCFF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <strong><span class="auto-style1">Simple Calculator</span></strong><br />
+            <br />
+            <span class="auto-style2">First Value:</span>
+            <asp:TextBox ID="firstValueTextBox" runat="server" OnTextChanged="firstValueTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <span class="auto-style2">Second Value: </span>
+            <asp:TextBox ID="secondValueTextBox" runat="server" OnTextChanged="secondValueTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="plusButton" runat="server" OnClick="plusButton_Click" Text="+" Width="29px" />
+&nbsp;<asp:Button ID="minusButton" runat="server" OnClick="Button2_Click" Text="-" Width="29px" />
+&nbsp;<asp:Button ID="multiplicationButton" runat="server" OnClick="multiplicationButton_Click" Text="*" Width="29px" />
+&nbsp;<asp:Button ID="devisionButton" runat="server" OnClick="Button4_Click" Text="/" Width="29px" />
+            <br />
+            <br />
+        </div>
+        <strong>
+        <asp:Label ID="returnLabel" runat="server" CssClass="auto-style3"></asp:Label>
+        </strong>
+    </form>
+</body>
+</html>
---
+++ Lesson 2 - Data Types- Variables and Operators/SimpleCalculatorChallenge/SimpleCalculatorChallenge/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculatorChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+            double myResult = double.Parse(firstValue) - double.Parse(secondValue);
+            returnLabel.Text = myResult.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+            double myResult = double.Parse(firstValue) / double.Parse(secondValue);
+            returnLabel.Text = myResult.ToString();
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+            double myResult = double.Parse(firstValue) + double.Parse(secondValue);
+            returnLabel.Text = myResult.ToString();
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+            double myResult = double.Parse(firstValue) * double.Parse(secondValue);
+            returnLabel.Text = myResult.ToString();
+        }
+
+        protected void firstValueTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void secondValueTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
