d45fa1c247a195ac07b58838472dec7c38eedc3b
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-11-05 01:37:39 -0700

    Add files via upload

---
+++ Lesson 3/ChallengeConditonalRadioButton/ChallengeConditonalRadioButton/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditonalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Prefernces<br />
+            <br />
+            <asp:RadioButton ID="pencilButton" runat="server" GroupName="noteTakingTool" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="penButton" runat="server" GroupName="noteTakingTool" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="phoneButton" runat="server" GroupName="noteTakingTool" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="tabletButton" runat="server" GroupName="noteTakingTool" Text="Tablet" />
+            <br />
+            <br />
+            <asp:Image ID="resultImage" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="OKButton" runat="server" OnClick="OKButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lesson 3/ChallengeConditonalRadioButton/ChallengeConditonalRadioButton/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditonalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OKButton_Click(object sender, EventArgs e)
+        {
+            
+            if (pencilButton.Checked)
+            {
+                resultLabel.Text = "You selected Pencil";
+                resultImage.ImageUrl = "pencil.png";
+            }
+            else if (penButton.Checked)
+            {
+                resultLabel.Text = "You selected Pen";
+                resultImage.ImageUrl = "pen.png";
+            }
+            else if (phoneButton.Checked)
+            {
+                resultLabel.Text = "You selected Phone";
+                resultImage.ImageUrl = "phone.png";
+            }
+            else if (tabletButton.Checked)
+            {
+                resultLabel.Text = "You selected Tablet";
+                resultImage.ImageUrl = "tablet.png";
+            }
+            else
+            {
+                resultLabel.Text = "Please select an option";
+   
+            }
+            
+        }
+    }
+}
