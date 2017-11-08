bb8c90de365167ed1eabdc3f3ae1e05d6ed71f9e
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-11-07 23:05:39 -0800

    Add files via upload

---
+++ Lesson 5 - Working with ASP.NET and Formatting Strings/CS-ASP_019/CS-ASP_019/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_019.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Loan Application Form<br />
+            <br />
+            Name:
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Phone Number:
+            <asp:TextBox ID="phoneTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Social Security Number:
+            <asp:TextBox ID="ssnTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Loan Origination Date:<br />
+            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Salary:
+            <asp:TextBox ID="salaryTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lesson 5 - Working with ASP.NET and Formatting Strings/CS-ASP_019/CS-ASP_019/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_019
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int ssn = int.Parse(ssnTextBox.Text);
+            double phone = double.Parse(phoneTextBox.Text);
+
+            //string result = String.Format("Thank you, {0}, for your business", nameTextBox.Text);
+            /*string result = String.Format("Thank you, {0}, for your business." +
+                "<br />Your Social Secirity Number is: {1:000-00-0000}" +
+                "<br />Your Phone number is: {2:(000)000-0000}", nameTextBox.Text, ssn, phone);*/
+
+            /*string result = String.Format("Thank you, {0}, for your business." +
+            "<br />Your Social Secirity Number is: {1:000-00-0000}" +
+            "<br />Your Phone number is: {2:(000)000-0000}" +
+            "<br />Loan Date: {3:ddd -- d, M, yy}",
+            nameTextBox.Text, 
+            ssn, 
+            phone,
+            Calendar1.SelectedDate);
+
+            resultLabel.Text = result;*/
+
+            double salary = double.Parse(salaryTextBox.Text);
+            string result = String.Format("Thank you, {0}, for your business." +
+            "<br />Your Social Secirity Number is: {1:000-00-0000}" +
+            "<br />Your Phone number is: {2:(000)000-0000}" +
+            "<br />Loan Date: {3:ddd -- d, M, yy}" +
+            "<br />Salary:{4:C} ",
+            nameTextBox.Text,
+            ssn,
+            phone,
+            Calendar1.SelectedDate,
+            salary);
+
+            resultLabel.Text = result;
+        }
+    }
+}
