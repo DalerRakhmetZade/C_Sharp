3d656131aaa15de9626784018f5ce0017d5a02cc
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-11-08 21:20:58 -0800

    Add files via upload

--- Lesson 5 - Working with ASP.NET and Formatting Strings/ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
+++ Lesson 5 - Working with ASP.NET and Formatting Strings/ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -28,9 +28,12 @@ namespace ChallengeEpicSpiesAssignment
                 totalAmountPaid = totalAmountPaid + 1000;
 
             Double daysBetween = newCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate).TotalDays;
-            DateTime startDate = previousCalendar.SelectedDate.AddDays(14); 
+            DateTime startDate = previousCalendar.SelectedDate.AddDays(14);
+            DateTime endDate = previousCalendar.SelectedDate.AddDays(21);
             if (daysBetween < 14)
             {
+                newCalendar.SelectedDate = startDate;
+                endDateCalendar.SelectedDate = endDate;
                 resultLabel.Text = String.Format("Error: Must allow at least two weeks between previous assignment and new assignment." +
                     "{0} can start New Assignment as soon as {1:MMMM dd, yyyy}", spyNameTextBox.Text, startDate);
             }
