86c8224f4dc5ecf91b4ff0a9851c03da7d309dd3
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-11-14 23:03:25 -0800

    Add files via upload

---
+++ Lesson 6 - Arrays and Iteration Statements/ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: xx-large;
+        }
+        .auto-style2 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="150px" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+            <br />
+            <strong><span class="auto-style2">Asset Performance Tracker</span><br class="auto-style1" />
+            </strong>
+            <br />
+            Asset Name:
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="electionsTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="actsTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lesson 6 - Arrays and Iteration Statements/ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                string[] assetName = new string[0];
+                ViewState.Add("Asset Name", assetName);
+
+                double[] elections = new double[0];
+                ViewState.Add("Elections Rigged", elections);
+
+                double[] acts = new double[0];
+                ViewState.Add("Acts of Subterfuge", acts);
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] assetName = (string[])ViewState["Asset Name"];
+
+            Array.Resize(ref assetName, assetName.Length + 1);
+            int newestItem = assetName.GetUpperBound(0);
+
+            assetName[newestItem] = nameTextBox.Text;
+            ViewState["Asset Name"] = assetName;
+
+
+
+            double[] elections = (double[])ViewState["Elections Rigged"];
+
+            Array.Resize(ref elections, elections.Length + 1);
+            int newestItem1 = elections.GetUpperBound(0);
+
+            elections[newestItem1] = double.Parse(electionsTextBox.Text);
+            ViewState["Elections Rigged"] = elections;
+
+
+
+            double[] acts = (double[])ViewState["Acts of Subterfuge"];
+
+            Array.Resize(ref acts, acts.Length + 1);
+            int newestItem2 = acts.GetUpperBound(0);
+
+            acts[newestItem] = double.Parse(actsTextBox.Text);
+            ViewState["Acts of Subterfuge"] = acts;
+
+
+
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0} <br /> Average Acts of Subterfuge per Asset: {1:N2} <br /> (Last Asset you Added: {2})", 
+                elections.Sum(),
+                acts.Average(),
+                assetName.Last());
+
+           
+        }
+    }
+}
