e1d7cfa9ea97b2880259b2c3bb194f6bdb4517c3
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-12-06 21:26:04 -0800

    Add files via upload

---
+++ Lesson 11- Working with Collections/ChallengeStudentCourses/ChallengeStudentCourses/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeStudentCourses
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+
+    }
+
+    
+}
