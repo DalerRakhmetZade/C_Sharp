9b8734403e1944ad6114de9350f9415485f4cf22
Daler Rakhmet-Zade <daler4ik@gmail.com>
2017-12-07 22:50:57 -0800

    Add files via upload

---
+++ Lesson 11- Working with Collections/MegaChallengeWar/MegaChallengeWar/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Card> Card { get; set; }
+
+        public Player()
+        {
+            Card = new List<Card>();
+        }
+    }
+}
